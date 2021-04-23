cb33cbca91e92b6fd5f7450b3bbe766ee221c06b
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-04-23 23:22:20 +0300

    infinite scroll attempt#1

--- src/components/ToReadListLeft/ToReadListLeft.js
+++ src/components/ToReadListLeft/ToReadListLeft.js
@@ -1,73 +1,115 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, useCallback } from "react";
 import { observer } from "mobx-react-lite";
-import { normalizeBooks  } from "./../../utils/utils";
-import {  fetchBooks,debounce } from "./../../utils/api";
+import { normalizeBooks } from "./../../utils/utils";
+import { fetchBooks, debounce } from "./../../utils/api";
 import { Spinner } from "./../../UI/Spinner/Spinner";
 import { BookItem } from "./../BookItem/BookItem";
-import { Button } from './../../UI/Button/Button';
-import { ReactComponent as SearchIcon } from '../../icons/search.svg';
+import { Button } from "./../../UI/Button/Button";
+import { ReactComponent as SearchIcon } from "../../icons/search.svg";
 import "./ToReadListLeft.css";
 
 export const ToReadListLeft = observer(({ booksState }) => {
-	const {
-		books,
-		selectedBookId,
-		setSelectedBookId,
-		updateBooks,
-	} = booksState;
-	
+	const { books, selectedBookId, setSelectedBookId, updateBooks } = booksState;
+
 	const [page, setPage] = useState(1);
-	const [inputValue, setInputValue] = useState('')
+	const [inputValue, setInputValue] = useState("");
 	const [isLoading, setLoading] = useState(false);
 	const [isLoaded, setLoaded] = useState(false);
 
-
 	const getBooks = () => {
-		if (inputValue === '') return
+		if (inputValue === "") return;
 		setLoading(true);
-		fetchBooks(inputValue, page).then(data => {
+		fetchBooks(inputValue, page).then((data) => {
 			updateBooks(normalizeBooks(data.docs));
 			setLoaded(true);
 			setLoading(false);
-		})
+		});
 	};
+
+	const onBookSelect = (idx) => () => setSelectedBookId(idx);
+
+	const onInputChangeHandler = (e) => setInputValue(e.target.value);
+
+	const onFetchBookHandler = () => getBooks();
+
+	const intersectionObserver = useRef();
+	const target = useRef();
+
+	const handleScroll = () => {
+		alert('haha')
+	}
+
+	// intersectionObserver.current = new IntersectionObserver(handleScroll);
+	// console.log(target)
+	// if (target) intersectionObserver.current.observe(target.current)
+
 	
-	const onBookSelect = idx => () => setSelectedBookId(idx)
+	const lastBookElementRef = useCallback((node) => {
+			// if (loading) return
+			// if (observer.current) observer.current.disconnect()
+			intersectionObserver.current = new IntersectionObserver((entries) => {
+				if (entries[0].isIntersecting) {
+					setPage((prevPage) => prevPage + 1);
+					alert("hi");
+				}
+			});
+			setPage((prevPage) => prevPage + 1);
+			alert("hi");
+			// if (node) intersectionObserver.current.observe(node)
+			observer.current.observe(node);
+		}, []);
 
-	const onInputChangeHandler = (e) => setInputValue(e.target.value)
-
-	const onFetchBookHandler = () => getBooks()
-
-	useEffect(() => debounce(getBooks), [inputValue])
-	
+	useEffect(() => debounce(getBooks), [inputValue]);
 
 	return (
 		<div className="left-container">
 			<header className="search">
-				<input onKeyDown = {onInputChangeHandler}className="search__input mr-10" type="text" placeholder="any book author" />
-				
-				{isLoading ? <Spinner /> : <Button onClick = {onFetchBookHandler}className = 'search__button'><SearchIcon/></Button>}
+				<input
+					onKeyDown={onInputChangeHandler}
+					className="search__input mr-10"
+					type="text"
+					placeholder="any book author"
+				/>
+
+				{isLoading ? (
+					<Spinner />
+				) : (
+					<Button onClick={onFetchBookHandler} className="search__button">
+						<SearchIcon />
+					</Button>
+				)}
 			</header>
 
-			<section className = 'book-list'>
+			<section className="book-list">
 				{isLoaded &&
-					books.map((book, idx) => (
-						<BookItem
-							onClick = {onBookSelect(idx)}
-							isSelected={idx === selectedBookId}
-							book={book}
-							key={book.id}
-						/>
-					))}
+					books.map((book, idx) => {
+						if (books.length === idx) return (
+								<BookItem
+									onClick={onBookSelect(idx)}
+									isSelected={idx === selectedBookId}
+									book={book}
+									key={book.id}
+									ref={lastBookElementRef}
+								/>
+							);
+						return (
+							<BookItem
+								onClick={onBookSelect(idx)}
+								isSelected={idx === selectedBookId}
+								book={book}
+								key={book.id}
+							/>
+						);
+					})}
 			</section>
 
-			<footer className = 'pagination'>
+			<footer className="pagination">
 				<div>
-					<span className = 'pagination__top'>Found: xxx  Start: xxx  Page size: xxx</span>	
+					<span className="pagination__top">Found: xxx Start: xxx Page size: xxx</span>
 				</div>
-				<div className = 'pagination__buttons'>
-					<Button className = 'pagination__button--left'>Prev</Button>
-					<Button className = 'pagination__button--right'>Next </Button>
+				<div className="pagination__buttons">
+					<Button className="pagination__button--left">Prev</Button>
+					<Button className="pagination__button--right">Next </Button>
 				</div>
 			</footer>
 		</div>
