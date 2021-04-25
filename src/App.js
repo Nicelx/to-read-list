0b2bfd7c6ba624ee8d0c2cf08f4f8229d0463d59
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-04-25 16:06:01 +0300

    adaptive +-

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { BookInfo } from "./components/BookInfo/BookInfo";
 import { ToReadListLeft } from './components/ToReadListLeft/ToReadListLeft';
 import { ToReadListRight } from "./components/ToReadListRight/ToReadListRight";
 import {booksState} from './store/books';
-import {SnackBar} from './UI/SnackBar/SnackBar';
+// import {SnackBar} from './UI/SnackBar/SnackBar';
 
 function App() {
 	return (
 		<div className="app-container">
 			<ToReadListLeft booksState = {booksState}/>
-			<SnackBar><BookInfo booksState = {booksState}/></SnackBar>
+			{/* <SnackBar><BookInfo booksState = {booksState}/></SnackBar> */}
+			<BookInfo booksState = {booksState}/>
 			<ToReadListRight booksState = {booksState}/>
 		</div>
 	);
